5b73e8869758369a11918f3a7019185ba3e7e6c1
Vai Wilmintje <Vai Wilmintje>
2016-12-18 20:52:13 +0700

    add position controller

---
+++ controllers/PositionController.js
@@ -0,0 +1,35 @@
+"use strict";
+const BaseController_1 = require("./BaseController");
+class PositionController extends BaseController_1.default {
+    constructor() {
+        super('Position');
+        this.includes.push({ model: this.models.Client, as: 'client' });
+    }
+    findByClient(clientId) {
+        return this.model.findAll({ where: { clientId: clientId }, include: this.includes });
+    }
+    findLatestByClient(clientId) {
+        return this.model.findOne({ where: { clientId: clientId }, include: this.includes, order: ['id', 'DESC'] });
+    }
+    saveNotStationary(data) {
+        return this.db.transaction((t) => {
+            return this.model.findOne({ where: { latitude: data.latitude, longitude: data.longitude }, transaction: t }).then(res => {
+                if (res)
+                    return false;
+                return this.model.insertOrUpdate(data, { transaction: t });
+            });
+        });
+    }
+    applyQuery(query) {
+        var where = {};
+        var options = { where: where, include: this.includes };
+        if (query['limit'] && query['skip']) {
+            options.limit = query['limit'];
+            options.offset = query['skip'];
+        }
+        return options;
+    }
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = new PositionController();
+//# sourceMappingURL=PositionController.js.map
---
+++ controllers/PositionController.ts
@@ -0,0 +1,44 @@
+﻿import * as sequelize from 'sequelize';
+import { IPosition } from '../models/Position';
+import { Instance } from '../models/Instance';
+import BaseController from './BaseController';
+
+class PositionController extends BaseController<IPosition>{
+    constructor() {
+        super('Position');
+        this.includes.push({ model: this.models.Client, as: 'client' });
+    }
+
+    findByClient(clientId: number): Promise<Instance<IPosition>[]> {
+        return this.model.findAll({ where: { clientId: clientId }, include: this.includes });
+    }
+
+    findLatestByClient(clientId: number): Promise<Instance<IPosition>> {
+        return this.model.findOne({ where: { clientId: clientId }, include: this.includes, order: ['id', 'DESC'] });
+    }
+
+    saveNotStationary(data: IPosition): Promise<Instance<boolean>> {
+        return this.db.transaction((t) => {
+            return this.model.findOne({ where: { latitude: data.latitude, longitude: data.longitude }, transaction: t }).then(res => {
+                if (res)
+                    return false;
+
+                return this.model.insertOrUpdate(data, { transaction: t });
+            });
+        });
+    }
+
+    applyQuery(query: any): any {
+        var where: sequelize.WhereOptions = {};
+        var options: sequelize.FindOptions = { where: where, include: this.includes };
+
+        if (query['limit'] && query['skip']) {
+            options.limit = query['limit'];
+            options.offset = query['skip'];
+        }
+
+        return options;
+    }
+}
+
+export default new PositionController();
