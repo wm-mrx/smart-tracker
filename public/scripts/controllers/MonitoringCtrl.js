c92508601098729565bc721ad13777ec8d32447e
Vai Wilmintje <Vai Wilmintje>
2016-12-19 11:03:44 +0700

    configure map and marker

--- public/scripts/controllers/MonitoringCtrl.js
+++ public/scripts/controllers/MonitoringCtrl.js
@@ -7,12 +7,22 @@ var SmartTracker;
                 this.$scope = $scope;
                 this.$state = $state;
                 this.principal = principal;
+                this.osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                });
+                this.satellite = L.tileLayer('https://api.mapbox.com/styles/v1/vampire13/civxpqmqe006z2jo5r3ata02a/tiles/256/{z}/{x}/{y}?access_token={apiKey}', {
+                    user: 'vampire13',
+                    apiKey: 'pk.eyJ1IjoidmFtcGlyZTEzIiwiYSI6ImNpdnhwa3M2azAyZHMydHRheHN4NDF6dXAifQ.inowv5XJuorxQA_fsWxciQ',
+                    mapId: 'mapbox.mapbox-streets-v7'
+                });
                 this.clients = [];
+                this.clientMarkers = [];
                 principal.identity().then((identity) => {
+                    this.initMap();
                     var socket = io.connect(SmartTracker.socketUrl);
                     socket.emit('set clients', null);
                     socket.on('update position', (data) => {
-                        var client = this.clients.filter(e => e.deviceId === data.deviceId)[0];
+                        var client = this.clients.filter(e => e.deviceId == data.deviceId)[0];
                         if (!client)
                             return;
                         console.log(data);
@@ -21,7 +31,7 @@ var SmartTracker;
                         $scope.$apply(() => {
                             for (var i = 0; i < clients.length; i++) {
                                 var client = new SmartTracker.Models.Client(clients[i]);
-                                var existingClient = this.clients.filter(e => e.id === client.id)[0];
+                                var existingClient = this.clients.filter(e => e.id == client.id)[0];
                                 if (!existingClient) {
                                     socket.emit('set latest position', client.device.serial);
                                     this.clients.push(client);
@@ -30,15 +40,24 @@ var SmartTracker;
                         });
                     });
                     socket.on('get latest position', (data) => {
-                        var client = this.clients.filter(e => e.deviceId === data.deviceId)[0];
-                        if (!client)
-                            return;
-                        console.log(data);
+                        $scope.$apply(() => {
+                            var position = new SmartTracker.Models.Position(data);
+                            var client = this.clients.filter(e => e.id == position.clientId)[0];
+                            var latlng = L.latLng(position.latitude, position.longitude);
+                            var existingClientMarker = this.clientMarkers.filter(e => e.client.id == client.id)[0];
+                            if (!existingClientMarker) {
+                                var newMarker = L.marker(latlng);
+                                newMarker['client'] = client;
+                                newMarker.addTo(this.map);
+                                return;
+                            }
+                            existingClientMarker.marker.setLatLng(latlng);
+                        });
                     });
                     socket.on('update client', (data) => {
                         $scope.$apply(() => {
                             var client = new SmartTracker.Models.Client(data);
-                            var existingClient = this.clients.filter(e => e.id === client.id)[0];
+                            var existingClient = this.clients.filter(e => e.id == client.id)[0];
                             if (!existingClient) {
                                 socket.emit('set latest position', client.device.serial);
                                 this.clients.push(client);
@@ -46,6 +65,11 @@ var SmartTracker;
                         });
                     });
                 });
+            }
+            initMap() {
+                this.map = L.map('map', { center: this.center, zoom: 12, zoomControl: false });
+                L.control.layers({ "Osm": this.osm, "Satellite": this.satellite }).addTo(this.map);
+                this.osm.addTo(this.map);
             }
         }
         MonitoringCtrl.$inject = ['$scope', '$state', 'principal'];
--- public/scripts/controllers/MonitoringCtrl.ts
+++ public/scripts/controllers/MonitoringCtrl.ts
@@ -6,23 +6,42 @@
         emit(event: string, data: any);
     }
 
+    interface IClientMarker {
+        client: Models.IClient;
+        marker: L.Marker;
+    }
+
     class MonitoringCtrl {
         map: L.Map;
         center: L.LatLng;
         clients: Models.IClient[];
+        clientMarkers: IClientMarker[];
+
+        osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        });
+
+        satellite = L.tileLayer('https://api.mapbox.com/styles/v1/vampire13/civxpqmqe006z2jo5r3ata02a/tiles/256/{z}/{x}/{y}?access_token={apiKey}', {
+            user: 'vampire13',
+            apiKey: 'pk.eyJ1IjoidmFtcGlyZTEzIiwiYSI6ImNpdnhwa3M2azAyZHMydHRheHN4NDF6dXAifQ.inowv5XJuorxQA_fsWxciQ',
+            mapId: 'mapbox.mapbox-streets-v7'
+        });
 
         static $inject = ['$scope', '$state', 'principal'];
 
         constructor(public $scope, public $state, public principal) {
             this.clients = [];
+            this.clientMarkers = [];
 
             principal.identity().then((identity) => {
+                this.initMap();
+
                 var socket = io.connect(socketUrl);
 
                 socket.emit('set clients', null);
 
                 socket.on('update position', (data) => {
-                    var client = this.clients.filter(e => e.deviceId === data.deviceId)[0];
+                    var client = this.clients.filter(e => e.deviceId == data.deviceId)[0];
 
                     if (!client)
                         return;
@@ -34,7 +53,7 @@
                     $scope.$apply(() => {
                         for (var i = 0; i < clients.length; i++) {
                             var client = new Models.Client(clients[i]);
-                            var existingClient = this.clients.filter(e => e.id === client.id)[0];
+                            var existingClient = this.clients.filter(e => e.id == client.id)[0];
 
                             if (!existingClient) {
                                 socket.emit('set latest position', client.device.serial);
@@ -45,18 +64,28 @@
                 });
 
                 socket.on('get latest position', (data) => {
-                    var client = this.clients.filter(e => e.deviceId === data.deviceId)[0];
+                    $scope.$apply(() => {
+                        var position = new Models.Position(data);
+                        var client = this.clients.filter(e => e.id == position.clientId)[0];
 
-                    if (!client)
-                        return;
+                        var latlng = L.latLng(position.latitude, position.longitude);
+                        var existingClientMarker = this.clientMarkers.filter(e => e.client.id == client.id)[0];
 
-                    console.log(data);
+                        if (!existingClientMarker) {
+                            var newMarker = L.marker(latlng);
+                            newMarker['client'] = client;
+                            newMarker.addTo(this.map);
+                            return;
+                        }
+
+                        existingClientMarker.marker.setLatLng(latlng);
+                    });
                 });
 
                 socket.on('update client', (data) => {
                     $scope.$apply(() => {
                         var client = new Models.Client(data);
-                        var existingClient = this.clients.filter(e => e.id === client.id)[0];
+                        var existingClient = this.clients.filter(e => e.id ==  client.id)[0];
 
                         if (!existingClient) {
                             socket.emit('set latest position', client.device.serial);
@@ -65,6 +94,12 @@
                     });
                 });
             });
+        }
+
+        initMap(): void {
+            this.map = L.map('map', { center: this.center, zoom: 12, zoomControl: false });
+            L.control.layers({ "Osm": this.osm, "Satellite": this.satellite }).addTo(this.map);
+            this.osm.addTo(this.map);
         }
     }
 
